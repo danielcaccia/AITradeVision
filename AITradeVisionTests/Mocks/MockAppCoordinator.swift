f67f2428bc29a9fdd18e501913db76d9387b9e33
Daniel Caccia <danielcaccia@icloud.com>
2025-04-07 19:41:48 +0200

    Add AuthCoordinatorTests

---
+++ AITradeVisionTests/Mocks/MockAppCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  MockAppCoordinator.swift
+//  AITradeVision
+//
+//  Created by Daniel Caccia on 07/04/25.
+//
+
+import Foundation
+@testable import AITradeVision
+
+class MockAppCoordinator: AppCoordinator {
+    override var currentFlow: Flow {
+        get { flow }
+        set { flow = newValue }
+    }
+
+    private var flow: Flow = .auth
+}
---
+++ AITradeVisionTests/Presentation/Coordinators/AuthCoordinatorTests.swift
@@ -0,0 +1,56 @@
+//
+//  AuthCoordinatorTests.swift
+//  AITradeVision
+//
+//  Created by Daniel Caccia on 07/04/25.
+//
+
+import XCTest
+import SwiftUI
+@testable import AITradeVision
+
+@MainActor
+final class AuthCoordinatorTests: XCTestCase {
+    func buildSUT() -> (AuthCoordinator, MockAppCoordinator) {
+        let mockAppCoordinator = MockAppCoordinator()
+        let sut = AuthCoordinator(appCoordinator: mockAppCoordinator)
+        return (sut, mockAppCoordinator)
+    }
+
+    // MARK: - Tests
+
+    func testInitialRouteIsLogin() {
+        // Arrange
+        let (sut, _) = buildSUT()
+        
+        // Assert
+        XCTAssertEqual(sut.route, .login)
+    }
+
+    func testSwitchToMainAppSetsAppCoordinatorFlowToMarket() {
+        // Arrange
+        let (sut, appCoordinator) = buildSUT()
+        
+        // Act
+        sut.switchToMainApp()
+        
+        // Arrange
+        XCTAssertEqual(appCoordinator.currentFlow, .market)
+    }
+
+    func testBuildCurrentViewLoginReturnsLoginView() {
+        // Arrange
+        let (sut, _) = buildSUT()
+        
+        // Act
+        let _ = sut.buildCurrentView(for: .login)
+    }
+
+    func testBuildCurrentViewRegisterReturnsRegisterView() {
+        // Arrange
+        let (sut, _) = buildSUT()
+        
+        // Act
+        let _ = sut.buildCurrentView(for: .register)
+    }
+}
